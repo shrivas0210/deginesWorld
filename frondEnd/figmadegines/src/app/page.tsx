0bfd5ddc9f444e9792741e0299dcc356b63869e9
shrivas0210 <sanjayshrivas0210@gmail.com>
2025-12-18 13:28:34 +0530

    Add

---
+++ frondEnd/figmadegines/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>         
+        </div>   
+    
+    </div>
+  );
+}
